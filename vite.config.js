16eb814124bea5a64cef8f52f6122a777d15d7b2
luisbusta7 <mbjsjrm57t@privaterelay.appleid.com>
2026-05-21 19:42:19 +0200

    Actualizando proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./", // Usa rutas relativas en los enlaces generados en el HTML
+  base: "/gathsession-landing/", 
 });
